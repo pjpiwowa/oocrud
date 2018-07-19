930ad1a52db92bcf2adb0f99a4163ea6ba77a3b2
Peter J. Piwowarski <pjpiwowa@svsu.edu>
2018-07-19 12:04:48 -0400

    initial (horrifying) commit

---
+++ customers.php
@@ -0,0 +1,14 @@
+<?php
+
+class Customers
+{
+	function lst()
+	{
+		echo "there are no customers, you lie\n";
+	}
+}
+
+$ct = new Customers();
+$ct->lst();
+
+?>
---
+++ read.php
@@ -0,0 +1,74 @@
+<?php 
+	require 'database.php';
+	$id = null;
+	if ( !empty($_GET['id'])) {
+		$id = $_REQUEST['id'];
+	}
+	
+	if ( null==$id ) {
+		header("Location: index.php");
+	} else {
+		$pdo = Database::connect();
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "SELECT * FROM customers where id = ?";
+		$q = $pdo->prepare($sql);
+		$q->execute(array($id));
+		$data = $q->fetch(PDO::FETCH_ASSOC);
+		Database::disconnect();
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div class="container">
+    
+    			<div class="span10 offset1">
+    				<div class="row">
+		    			<h3>Read a Customer</h3>
+		    		</div>
+		    		
+	    			<div class="form-horizontal" >
+					  <div class="control-group">
+					    <label class="control-label">Name</label>
+					    <div class="controls">
+						    <label class="checkbox">
+						     	<?php echo $data['name'];?>
+						    </label>
+					    </div>
+					  </div>
+					  <div class="control-group">
+					    <label class="control-label">Email Address</label>
+					    <div class="controls">
+					      	<label class="checkbox">
+						     	<?php echo $data['email'];?>
+						    </label>
+					    </div>
+					  </div>
+					  <div class="control-group">
+					    <label class="control-label">Mobile Number</label>
+					    <div class="controls">
+					      	<label class="checkbox">
+						     	<?php echo $data['mobile'];?>
+						    </label>
+					    </div>
+					  </div>
+					    <div class="form-actions">
+						  <a class="btn" href="index.php">Back</a>
+					   </div>
+					
+					 
+					</div>
+				</div>
+				
+    </div> <!-- /container -->
+  </body>
+</html>
