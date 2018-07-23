80cb7bec07ed05149df96d212992cb3805971955
Peter J. Piwowarski <pjpiwowa@svsu.edu>
2018-07-22 20:54:58 -0400

    slightly less silly title

--- index.php
+++ index.php
@@ -8,6 +8,6 @@ $db = new Database($DB_NAME, $DB_HOST, $DB_USER, $DB_PASS);
 $tbl = new cruddy_table($db, "customers", array("name" => "Name", "email" => "Email Address", "mobile" => "Phone Number"));
 
 echo cruddy_page(in_div("<a href=\"https://github.com/pjpiwowa/oocrud\">Source Code</a>") .
-                 in_div("<h3>Dumpster Fire CRUD Grid</h3>", "row") .
+                 in_div("<h3>Horrible Yet Object Oriented CRUD Grid</h3>", "row") .
                  in_div(in_p("<a href=\"create.php\" class=\"btn btn-success\">Create</a>") .
                  $tbl->dump(), "row"));
